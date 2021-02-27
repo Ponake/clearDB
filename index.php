cc403c6369335cb0c825060935b3ba3a3c62644c
Ponake <74523071+Ponake@users.noreply.github.com>
2021-02-27 18:36:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	$server ="us-cdbr-east-03.cleardb.com";
+	$username = "b3cc1c5ad0af56";
+	$password = "87d9d6df";
+	$dbname = "heroku_4076c8df474b3c7";
+	
+	$connect =  new mysqli($server, $username, $password, $dbname);
+	
+	if ($connect->connect_error){
+		die("Something wrong.:" . $connect->connect_error);
+	}
+	
+	$sql = "SELECT * FROM user";
+	$result = $connect->query($sql);
+	
+		while($row = $result->fetch_assoc());
+		echo $row['username'];
+		echo "<br>";
+		endwhile
+?>
